36c7fa6c893b3b9daebbba025fe32bade2be9878
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-05-24 23:39:37 -0300

    Position Class created

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import boardGame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+    }
+}
